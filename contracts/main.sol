00996e3dc8a963eba57acd162e7ed2f7400b2420
henrygagnier <101131194+henrygagnier@users.noreply.github.com>
2023-06-07 06:45:25 -0400

    Update main.sol

--- contracts/main.sol
+++ contracts/main.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.10;
 
 contract Blockcharity {
-    //Tracking of donations - Done
+    //Tracking of donations - DONE
     mapping(address => uint256) public amountDonated;
     mapping(address => bool) public hasDonated;
     address public owner;
@@ -23,7 +23,7 @@ contract Blockcharity {
         }
     }
 
-    //Staff  - Done
+    //Staff  - DONE
 
     mapping(address => bool) staff;
 
@@ -43,7 +43,7 @@ contract Blockcharity {
         staff[newStaff] = true;
     }
 
-    //Organizations - Need Review
+    //Organizations - DONE
 
     struct Organization {
         address owner;
@@ -78,7 +78,7 @@ contract Blockcharity {
 
     }
 
-    //Donate - Done
+    //Donate - DONE
 
     function donate(uint256 id) public payable {
         require(msg.value > 0, "Cannot donate nothing");
@@ -86,7 +86,7 @@ contract Blockcharity {
         payable(Organizations[id].owner).transfer(msg.value);
     }
 
-    //Grants
+    //Grants - DONE
 
     uint256 public inUseAmt;
 
@@ -95,6 +95,7 @@ contract Blockcharity {
         uint256 amount;
         string description;
         uint256 endDate;
+        bool hasBeenExecuted;
     }
 
     Grant[] public grants;
@@ -107,26 +108,40 @@ contract Blockcharity {
         require(Organizations[id].owner == msg.sender); //Sender is owner
         require(Organizations[id].class == 1); //Verified
         require((address(this).balance - inUseAmt) / 10 >= amount_); //Cannot request too much
+        require(amount_ != 0);
         inUseAmt += amount_;
         votesForYes[grants.length] = 0;
         votesForNo[grants.length] = 0;
-        grants.push(Grant(id, amount_, description_, block.timestamp + 1 weeks));
+        grants.push(Grant(id, amount_, description_, block.timestamp + 1 weeks, false));
     }
 
-    function vote(uint256 index, bool vote) public {
+    function vote(uint256 index, bool vote_) public {
         require(grants[index].endDate != 0, "Not valid proposal");
         require(hasVoted[index][msg.sender] == false, "Already voted");
         require(Locks[msg.sender].value != 0, "No voting power");
         require(grants[index].endDate > block.timestamp, "Proposal has ended");
         hasVoted[index][msg.sender] = true;
-        if (vote == true) {
+        if (vote_ == true) {
             votesForYes[index] += Locks[msg.sender].value;
         } else {
             votesForNo[index] += Locks[msg.sender].value;
         }
     }
 
-    //Create Vote Lock - Unfinished
+    function execute(uint256 index) public {
+        require(grants[index].amount != 0); //Grant exists
+        require(grants[index].endDate < block.timestamp); //Ended proposal
+        require(grants[index].hasBeenExecuted == false);
+        grants[index].hasBeenExecuted = true;
+        uint256 value = grants[index].amount;
+        if (votesForYes[index] > votesForNo[index]) {
+            address recipient = Organizations[grants[index].organizationId].owner;
+            payable(recipient).transfer(value);
+        }
+        inUseAmt -= value;
+    }
+
+    //Create Vote Lock - DONE
 
     struct Lock {
         uint256 end;
